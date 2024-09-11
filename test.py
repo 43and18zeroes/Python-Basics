01588e706ac918d0d7bbbb406753403dd316664c
Christoph <c.wagner.germany@gmail.com>
2024-09-11 05:15:39 +0200

    float2

--- test.py
+++ test.py
@@ -5,3 +5,14 @@ print(bool("")) # False
 print(int("1.5"))
 
 print(int(float("1.5")))
+
+int(42.5) # 42
+float(-1) # -1.0
+str(0.5) # "0.5"
+bool(0.001) # True
+str("False") # Fehlermeldung
+int(False) # 0
+float("10") # 10.0
+bool('0') # False
+int("False") # 0
+float("True") # 1.0
