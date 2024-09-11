644706d62f0aba7189528ca947cdbb6451146948
Christoph <c.wagner.germany@gmail.com>
2024-09-11 05:52:56 +0200

    float7

--- test.py
+++ test.py
@@ -1,29 +1,8 @@
-print(bool(0.1)) # True
-print(bool(-0.1)) # True
-print(bool("")) # False
+name = "Florian"
 
-print(int("1.5"))
+print(name[-2]) # a
 
-print(int(float("1.5")))
+def reverse_string(s):
+    return s[::-1]
 
-int(42.5) # 42
-float(-1) # -1.0
-str(0.5) # "0.5"
-bool(0.001) # True
-str("False") # 'False'
-int(False) # 0
-float("10") # 10.0
-bool('0') # False
-int("False") # Fehlermeldung weil String zu Int
-float("True") # Fehlermeldung weil String zu Float
-
-print(
-    """
-    _____    __________________ .___.___  ___________            __   
-    /  _  \  /   _____/\_   ___ \|   |   | \_   _____/___   _____/  |_ 
-    /  /_\  \ \_____  \ /    \  \/|   |   |  |    __)/  _ \ /    \   __\
-    /    |    \/        \\     \___|   |   |  |     \(  <_> )   |  \  |  
-    \____|__  /_______  / \______  /___|___|  \___  / \____/|___|  /__|  
-            \/        \/         \/               \/             \/      
-    """
-)
+print(reverse_string("Advanced Strings"))
