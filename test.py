ff990c5b103eeb2e3c79208d1245db80b11e8a0d
Christoph <c.wagner.germany@gmail.com>
2024-09-11 06:59:28 +0200

    float13

--- test.py
+++ test.py
@@ -1,22 +1,10 @@
-def string_compression(s):
-    compressed = []
-    count = 1
-    for i in range(1, len(s)):
-        if s[i] == s[i - 1]:
-            count += 1
-        else:
-            compressed.append(s[i - 1] + str(count))
-            count = 1
-    compressed.append(s[-1] + str(count))  # Add last character
-    return ''.join(compressed)
+from collections import Counter
 
-print(string_compression("aaabbccddddeee"))
+def are_anagrams(s1, s2):
+    return Counter(s1) == Counter(s2)
+
+print(are_anagrams("listen", "silent"))
 
 
-
-a = "Hallo"
-b = " "
-c = "Python"
-d = "!"
-
-print(d * 3) # !!!
+print(dir(str))
+# ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
