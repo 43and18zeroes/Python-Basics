f95afdcf1514429fae090519e629cf3e1f624aeb
Christoph <c.wagner.germany@gmail.com>
2024-09-11 05:27:12 +0200

    float3

--- test.py
+++ test.py
@@ -10,9 +10,11 @@ int(42.5) # 42
 float(-1) # -1.0
 str(0.5) # "0.5"
 bool(0.001) # True
-str("False") # Fehlermeldung
+str("False") # 'False'
 int(False) # 0
 float("10") # 10.0
 bool('0') # False
-int("False") # 0
-float("True") # 1.0
+int("False") # Fehlermeldung weil String zu Int
+float("True") # Fehlermeldung weil String zu Float
+
+type
