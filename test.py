d52570016c7d2a09d21663a05298b154561d1f7c
Christoph <c.wagner.germany@gmail.com>
2024-09-12 15:44:41 +0200

    str10

--- test.py
+++ test.py
@@ -14,20 +14,19 @@ passwort1 = "abc123"
 passwort2 = "Pass Wort"
 passwort3 = "u1tr4g3h31m "
 
-print(passwort1.upper())
-print(passwort2.lower())
-print(passwort3.islower())
-print(passwort2.isupper())
-print(passwort1.zfill(8))
-print(passwort2.strip())
-print(len(passwort3))
-print(passwort1.isalpha())
-print(passwort1[3:].isnumeric())
-print("a;b;c;d;e".split(';'))
-print("01.23.45.67.89".split(';'))
-print(passwort2.replace("Pass",'.'))
-print(passwort3.count('3'))
-print(passwort2.count('s'))
-
-print(passwort3.find(2+2))
-print(passwort1.index("4"))
+print(passwort1.upper()) # ABC123
+print(passwort2.lower()) # pass wort
+print(passwort3.islower()) # True
+print(passwort2.isupper()) # False
+print(passwort1.zfill(8)) # 00abc123
+print(passwort2.strip()) # Pass Wort
+print(len(passwort3)) # 12
+print(passwort1.isalpha()) # False
+print(passwort1[3:].isnumeric()) # True
+print("a;b;c;d;e".split(';')) # [a, b, c, d, e]
+print("01.23.45.67.89".split(';')) # Error
+print(passwort2.replace("Pass",'.')) # . Wort
+print(passwort3.count('3')) # 2
+print(passwort2.count('s')) # 2
+print(passwort3.find(2+2)) # 4
+print(passwort1.index("4")) # -1
