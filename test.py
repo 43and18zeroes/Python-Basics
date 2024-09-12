17ebcf30f90adaa0eabad3195563f5426dc6dfc0
Christoph <c.wagner.germany@gmail.com>
2024-09-12 17:13:17 +0200

    str12

--- test.py
+++ test.py
@@ -6,17 +6,22 @@ def are_anagrams(s1, s2):
 print(are_anagrams("listen", "silent"))
 
 
-print(dir(str))
-# ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+print(30/15) # 2.0 Float Division
+
+print(30//15) # 2.0 Integer Division
+
+print(7//2) # 3 Alles nach dem Komma wird abgeschnitten
+
+print(7//8) # 0 Alles nach dem Komma wird abgeschnitten
 
 
 
-print("passw0r7".upper()) # PASSW0R7
-print("Anime".zfill(11)) # 000000Anime
-print("florian".capitalize()) # Florian
-print("Kaguya".lower()) # kaguya
-print("0123456789"[::-1]) # 9876543210
 
 
 
-print("0123456789"[0:9:-1]) # 9876543210
+
+
+
+
+
+
