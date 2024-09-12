dea373f7a77e66a304c7ed1b1bb10733159dc35b
Christoph <c.wagner.germany@gmail.com>
2024-09-12 15:36:20 +0200

    str9

--- test.py
+++ test.py
@@ -9,62 +9,25 @@ print(are_anagrams("listen", "silent"))
 print(dir(str))
 # ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
 
-print("Florian".zfill(10)) # 000Florian
 
-print("Florian".upper()) # FLORIAN
+passwort1 = "abc123"
+passwort2 = "Pass Wort"
+passwort3 = "u1tr4g3h31m "
 
-print("Florian".lower()) # florian
+print(passwort1.upper())
+print(passwort2.lower())
+print(passwort3.islower())
+print(passwort2.isupper())
+print(passwort1.zfill(8))
+print(passwort2.strip())
+print(len(passwort3))
+print(passwort1.isalpha())
+print(passwort1[3:].isnumeric())
+print("a;b;c;d;e".split(';'))
+print("01.23.45.67.89".split(';'))
+print(passwort2.replace("Pass",'.'))
+print(passwort3.count('3'))
+print(passwort2.count('s'))
 
-print("florian".capitalize()) # Florian
-
-print("florian".isupper()) # False
-
-print("florian".islower()) # True
-
-print("1234".isnumeric()) # True
-
-print("florian".isalpha()) # True
-
-print("Florian;Pamela;Mia;Tokage".split(';')) # ['Florian', 'Pamela', 'Mia', 'Tokage']
-
-print("Florian;Pamela;Mia;Tokage".split(';')) # ['Florian', 'Pamela', 'Mia', 'Tokage']
-
-print("Florian\nPamela\nMia\nTokage") # Florian
-# Pamela
-# Mia
-# Tokage
-
-print("Florian\nPamela\nMia\nTokage".splitlines()) # ['Florian', 'Pamela', 'Mia', 'Tokage']
-
-print("Florian\nPamela\nMia\nTokage".split("\n")) # ['Florian', 'Pamela', 'Mia', 'Tokage']
-
-print("   Florian   ".strip()) # Florian (like trim() in JS)
-
-print("   Florian   D   ".replace(" ", "")) # FlorianD
-
-print("   Florian   D   ".replace("o", ".")) #    Fl.rian   D
-
-print("Florian D".replace("Florian", "Pamela")) # Pamela D
-
-print("Florian".replace("ia", "uo")) # Floruon
-
-print("Wasserfall".count("s")) # 2
-
-print("Wasserfall".index("a")) # 1
-
-# Substring
-print("Wasserfall".index("fall")) # 6
-
-print("Wasserfall".find("fall")) # 6
-
-print("Wasserfall".find("o")) # -1 (nicht vorhanden)
-
-print("Wasserfall".find("o")) # -1 (nicht vorhanden)
-
-# keyword in (keine Methode)
-print("a" in "Wasserfall") # True
-
-
-
-
-
+print(passwort3.find(2+2))
+print(passwort1.index("4"))
