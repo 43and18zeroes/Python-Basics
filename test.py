cd7edf17e82fe391da2601215b0a32ac2cf8cc5b
Christoph <c.wagner.germany@gmail.com>
2024-09-13 19:23:49 +0200

    arith13

--- test.py
+++ test.py
@@ -7,12 +7,3 @@ harmonic_mean([1, 2, 4])  # Output: 1.7142857142857142
 
 
 
-print(2 < 3 and not 2 > 5) # True
-
-print(not True ^ False or 3 == 2 + 1) # True
-
-print(not not not 2 % 5 == 7 % 5) # False, 2 % 5 = 2
-
-print(True and False ^ True and False) # False
-
-print(True ^ False ^ 0 ^ 1 ^ (2 > 3)) # 0
