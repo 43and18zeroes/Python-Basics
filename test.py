7d2c57d5648481c99d66994fc7935e9848c3dcaf
Christoph <c.wagner.germany@gmail.com>
2024-09-13 18:04:44 +0200

    arith7

--- test.py
+++ test.py
@@ -1,25 +1,15 @@
-import numpy as np
-
-def fibonacci_matrix(n):
-    def matrix_mult(A, B):
-        return [[A[0][0] * B[0][0] + A[0][1] * B[1][0],
-                 A[0][0] * B[0][1] + A[0][1] * B[1][1]],
-                [A[1][0] * B[0][0] + A[1][1] * B[1][0],
-                 A[1][0] * B[0][1] + A[1][1] * B[1][1]]]
-
-    def matrix_pow(M, p):
-        if p == 1:
-            return M
-        if p % 2 == 0:
-            half_pow = matrix_pow(M, p // 2)
-            return matrix_mult(half_pow, half_pow)
+def prime_factors(n):
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
         else:
-            return matrix_mult(M, matrix_pow(M, p - 1))
-
-    F = [[1, 1], [1, 0]]
-    return matrix_pow(F, n)[0][1]
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
 # Example
-fibonacci_matrix(10)  # Output: 55 (10th Fibonacci number)
-
-
+prime_factors(100)  # Output: [2, 2, 5, 5]
