ca76e571b84bd1e7fb5e72755d97028b5988b9f1
Christoph <c.wagner.germany@gmail.com>
2024-09-13 17:35:28 +0200

    arith

--- test.py
+++ test.py
@@ -1,30 +1,29 @@
-print(12 * 12) #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
-print() #
+i = 0
+i += 1
+print(i) # 1
+
+i -= 1
+print(i) # 0
+
+i = 5
+i /= 2
+print(i) # 2.5
+
+i = 5
+i //= 2
+print(i) # 2
+
+a = 5
+a **= 2
+print(a) # 25
+
+b = 10
+b %= 3
+print(b) # 1
+
+
+
+
+
+
+
