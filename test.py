c7eef2a35d9b688a6ddf0cab87ac4ee261cf2d67
Christoph <c.wagner.germany@gmail.com>
2024-09-14 14:57:46 +0200

    str3

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
-def harmonic_mean(numbers):
-    return len(numbers) / sum(1 / num for num in numbers)
+passwort = input("Gib dein Passwort ein: ")
 
-# Example
-harmonic_mean([1, 2, 4])  # Output: 1.7142857142857142
-
-
-
-
-a = 2.5
-b = -5
-c = 20
-d = "Hallo"
-e = "Python"
-
-16**(a - 2) # 4.0
-d**c # Typeerror
-20**(2 * a + b) # 1.0 weil 20 hoch 0 = 1 weil Mathe
+if passwort == "abc123":
+    print("Dein Passwort ist korrekt")
