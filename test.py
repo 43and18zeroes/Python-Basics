12270b8a6c7269299648a1d7905789abc472f0b4
Christoph <c.wagner.germany@gmail.com>
2024-09-15 14:39:03 +0200

    str2

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-a = 2.5
-b = -5
-c = 20
-d = "Hallo"
-e = "Python"
+teilnehmerliste = []
 
-
-16**(a - 2) # 4.0
-d**c # Typeerror
-20**(2 * a + b) # 1.0 weil 20 hoch 0 = 1 weil Mathe
-
-2 + 2.0
+while (teilnehmer := input("Bitte gib einen Teilnehmer ein (X): ")) != "X":
+    teilnehmerliste.append(teilnehmer)
+        
+print(teilnehmerliste)
