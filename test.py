f0c0a633c855a362a2cfdac8fa43c45cdbdf63c9
Christoph <c.wagner.germany@gmail.com>
2024-09-15 14:48:09 +0200

    str3

--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-teilnehmerliste = []
-
-while (teilnehmer := input("Bitte gib einen Teilnehmer ein (X): ")) != "X":
-    teilnehmerliste.append(teilnehmer)
-        
-print(teilnehmerliste)
+gerade = [0, 2, 4, 6, 8, 10]
+print(gerade[0:3]) # [0, 2, 4]
