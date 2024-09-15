5aad7a15093bdd38e48ef45755a83a80c5dd353d
Christoph <c.wagner.germany@gmail.com>
2024-09-15 14:31:31 +0200

    str1

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-for number in numbers:
-    if number == 3:
-        break
-    print(number)
+a = 2.5
+b = -5
+c = 20
+d = "Hallo"
+e = "Python"
+
+
+16**(a - 2) # 4.0
+d**c # Typeerror
+20**(2 * a + b) # 1.0 weil 20 hoch 0 = 1 weil Mathe
+
+2 + 2.0
