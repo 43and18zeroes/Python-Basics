386108837f6a96b05c03802809d901582eb75de2
Christoph <c.wagner.germany@gmail.com>
2024-09-15 16:12:40 +0200

    str8

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-zahlen = ["F", "l", "o"]
+print(()) # (), leeres Tupel
 
-print("".join(zahlen)) # Flo
+test_tupel = (1, 2, 3)
+print(test_tupel[1]) # 2
+print(test_tupel[-2]) # 2
+print(test_tupel[-3]) # 1
+
+
+def addieren(a, b):
+    summe = a + b
+    return a, b, summe
+
+ergebnis = addieren(1, 4)
+print(ergebnis) # (1, 4, 5), ein Tupel
+print(type(ergebnis)) # <class 'tuple'>
+
+
+def addieren2(a, b):
+    summe = a + b
+    return summe
+
+ergebnis = addieren2(1, 4)
+print(ergebnis) # 5, ein Integer
+print(type(ergebnis)) # <class 'int'>
