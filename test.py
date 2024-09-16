8bf075d7e13b316c910b2e58ed3acd9490cf3e24
Christoph <c.wagner.germany@gmail.com>
2024-09-16 18:19:32 +0200

    sets9

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-def addieren(a, b):
-    summe = a + b
-    return summe
-
-s = addieren(10, 32)
-print(f"Summe: {s}") # Summe: 42
+def lotto_spielen(name, *lottozahlen):
+    print(f"Hallo {name}") # Hallo Florian
+    print(f"Deine Zahlen lauten: {lottozahlen}") # Deine Zahlen lauten: (4, 8, 15, 16, 23, 42)
+    
+lotto_spielen("Florian", 4, 8 , 15, 16, 23, 42)
