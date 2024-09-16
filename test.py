a36c238e1fbbc306557925103c72c89130a4455c
Christoph <c.wagner.germany@gmail.com>
2024-09-16 16:42:10 +0200

    sets2

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
-set_var = {12, 28, 38, True, 29, "Florian"}
-print(set_var) # {'Florian', True, 29, 38, 12, 28} Sets haben keine feste Reihenfolge
+zahlen = {1, 4, 6, 8}
 
-set_var2 = {1, 2, 1}
-print(set_var2) # {1, 2} Es wird kein Fehler geworfen, das Duplikat wird einfach nicht berücksichtigt
+zahlen.add(-1) # {1, 4, 6, 8, -1}
+print(zahlen)
 
-set_var3 = {1, 2, 3}
-print(len(set_var3)) # 3
+zahlen.remove(1)
+print(zahlen) # {4, 6, 8, -1}
 
-set_var4 = {1, 2, 1}
-print(len(set_var4)) # 2 Duplikate werden nicht mitgezählt
+kopie = zahlen.copy()
+print(kopie)
 
-liste = [42, "Florian", True]
-print(len(liste))
+zahlen.clear()
+print(zahlen) # set()
