dcb9986b10c8b06c7b742c3bcfab5c924291302f
Christoph <c.wagner.germany@gmail.com>
2024-09-16 17:26:48 +0200

    sets6

--- test.py
+++ test.py
@@ -9,5 +9,31 @@ print(passwort_hashes.get("ultrageheim", "456432185498768546513245679874654")) #
 passwort_hashes["abc123"] = "overwrite"
 print(passwort_hashes) # {'abc123': 'overwrite', '1337': 'e4u57913dfkgjst8kj45h234jkh2', 'love': 'e4u57913dfkgjst8kj45h234jkh2'}
 
-passwort_hashes["Eva"] = "1234" # {'abc123': 'overwrite', '1337': 'e4u57913dfkgjst8kj45h234jkh2', 'love': 'e4u57913dfkgjst8kj45h234jkh2', 'Eva': '1234'}
-print(passwort_hashes)
+passwort_hashes["Eva"] = "1234"
+print(passwort_hashes) # {'abc123': 'overwrite', '1337': 'e4u57913dfkgjst8kj45h234jkh2', 'love': 'e4u57913dfkgjst8kj45h234jkh2', 'Eva': '1234'}
+
+passwort_hashes.update({"1337" : "4321"})
+print(passwort_hashes) # {'abc123': 'overwrite', '1337': '4321', 'love': 'e4u57913dfkgjst8kj45h234jkh2', 'Eva': '1234'}
+
+del passwort_hashes["love"]
+print(passwort_hashes) # {'abc123': 'overwrite', '1337': '4321', 'Eva': '1234'}
+
+del passwort_hashes["Max"] # nichts passiert, es wird kein Fehler geworfen
+
+kopie = passwort_hashes.copy()
+print(kopie) # {'abc123': 'overwrite', '1337': '4321', 'Eva': '1234'}
+
+print(passwort_hashes.keys()) # dict_keys(['abc123', '1337', 'Eva'])
+
+print(type(passwort_hashes.keys())) # <class 'dict_keys'>
+
+print(type(list(passwort_hashes.keys()))) # <class 'list'>
+
+print(passwort_hashes.values()) # dict_values(['overwrite', '4321', '1234'])
+
+print(type(passwort_hashes.values())) # <class 'dict_values'>
+
+print(type(list(passwort_hashes.values()))) # <class 'list'>
+
+
+
