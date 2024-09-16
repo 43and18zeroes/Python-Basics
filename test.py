100f59b6ebab83c57a29c09e8ea568262ff31869
Christoph <c.wagner.germany@gmail.com>
2024-09-16 16:48:36 +0200

    sets2

--- test.py
+++ test.py
@@ -6,8 +6,10 @@ print(zahlen)
 zahlen.remove(1)
 print(zahlen) # {4, 6, 8, -1}
 
-kopie = zahlen.copy()
+kopie = zahlen.copy() # {8, 4, 6, -1}
 print(kopie)
+
+print(list(zahlen)) # [4, 6, 8, -1]
 
 zahlen.clear()
 print(zahlen) # set()
