ac37fa5b1281270dc4bbf256f562ba9e782b35af
Christoph <c.wagner.germany@gmail.com>
2024-09-16 18:30:59 +0200

    sets10

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-def lotto_spielen(name, *lottozahlen):
-    print(f"Hallo {name}") # Hallo Florian
-    print(f"Deine Zahlen lauten: {lottozahlen}") # Deine Zahlen lauten: (4, 8, 15, 16, 23, 42)
-    
-lotto_spielen("Florian", 4, 8 , 15, 16, 23, 42)
+def kwargs_test(**test):
+    print(test)
+
+kwargs_test({"the_key" : "the_value"})
