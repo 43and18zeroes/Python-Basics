1adee06b78a8de8b499548aef733d7e52ecf35bb
Christoph <c.wagner.germany@gmail.com>
2024-09-16 17:13:11 +0200

    sets5

--- test.py
+++ test.py
@@ -3,3 +3,11 @@ passwort_hashes = {
     "1337" : "e4u57913dfkgjst8kj45h234jkh2",
     "love" : "e4u57913dfkgjst8kj45h234jkh2"
 }
+
+print(passwort_hashes.get("ultrageheim", "456432185498768546513245679874654")) # 456432185498768546513245679874654, Default Wert
+
+passwort_hashes["abc123"] = "overwrite"
+print(passwort_hashes) # {'abc123': 'overwrite', '1337': 'e4u57913dfkgjst8kj45h234jkh2', 'love': 'e4u57913dfkgjst8kj45h234jkh2'}
+
+passwort_hashes["Eva"] = "1234" # {'abc123': 'overwrite', '1337': 'e4u57913dfkgjst8kj45h234jkh2', 'love': 'e4u57913dfkgjst8kj45h234jkh2', 'Eva': '1234'}
+print(passwort_hashes)
