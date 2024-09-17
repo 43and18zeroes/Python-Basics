ef7d7ba4ce9b8e25c2d4effaa476d64a0a7ccc08
Christoph <c.wagner.germany@gmail.com>
2024-09-17 17:19:33 +0200

    dic4

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-def teilnehmer(**daten):
-    vorname = daten.get("vorname")
-    nachname = daten.get("nachname")
-    alter = daten.get("alter", 18)
-    print(f"{vorname} {nachname}; {alter}") # Florian Dalwigk, 28
-    
-teilnehmer(vorname="Florian", nachname="Dalwigk", alter=28)
+def daten_erfassen(id, vorname, nachname, **daten):
+    pass
+
+daten_erfassen(42, "Florian", "Dalwigk", email="info@info.de")
