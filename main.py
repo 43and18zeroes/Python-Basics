1e5b530239a08148931b98e30c80df937102db01
Christoph <c.wagner.germany@gmail.com>
2024-09-18 15:41:01 +0200

    obj6

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 class Pokemon:
     
-    def __init__(self, n, level):
-        self.n = n
-        self.__level = level
-        self.vorstellen()
+    def __init__(self, name):
+        self.__n = name
+        self.__lebenspunkt = 42
+        self.__level = 1
         
     def vorstellen(self):
-        print(f"{self.n}, {self.n}!")
+        print(f"{self.__n}, {self.__n}!")
         
-    def get_level(self):
+    def zeige_lebenspunkt(self):
+        return self.__lebenspunkt
+    
+    def zeige_level(self):
         return self.__level
-        
+                
 if __name__ == "__main__":
-    bisasam = Pokemon("Bisasam", 0) # Bisasam, Bisasam!
+    p1 = Pokemon("Pikachu")
+    p1 = Pokemon("Bisasam")
+    
+    print(p1.zeige_level()) # 1
