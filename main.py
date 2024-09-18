f42e4a95f765c21c376ca81e340eda07aedcab30
Christoph <c.wagner.germany@gmail.com>
2024-09-18 18:15:36 +0200

    obj11

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ try:
     print(liste[index])
 except IndexError:
     print("Der Zugriff ist fehlgeschlagen.")
+    
+try:
+  x = int(input("Gib eine Zahl ein: "))
+  y = int(input("Gib eine andere Zahl ein: "))
+  result = x / y
+except ValueError:
+  print("Bitte gib nur Zahlen ein.")
+except ZeroDivisionError:
+  print("Division durch Null ist nicht erlaubt!")
