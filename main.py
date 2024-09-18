b76ff1d3cda835430b00d727de887f6ea18bc1a5
Christoph <c.wagner.germany@gmail.com>
2024-09-18 16:03:39 +0200

    obj7

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ class Pokemon:
     
     def __init__(self, name):
         self.__n = name
-        self.__lebenspunkt = 42
+        self.__lebenspunkte = 42
         self.__level = 1
         
     def vorstellen(self):
         print(f"{self.__n}, {self.__n}!")
         
-    def zeige_lebenspunkt(self):
-        return self.__lebenspunkt
+    def zeige_lebenspunkte(self):
+        return self.__lebenspunkte
     
     def zeige_level(self):
-        return self.__level
+        print(f"{self.__n} :: {self.__level}")
+    
+    def entwickeln(self):
+        self.__level += 1
+        
+    def attackieren(self, other, schaden):
+        other.__lebenspunkte -= schaden
+        
                 
 if __name__ == "__main__":
     p1 = Pokemon("Pikachu")
-    p1 = Pokemon("Bisasam")
+    p2 = Pokemon("Bisasam")
     
-    print(p1.zeige_level()) # 1
+    p1.attackieren(p2, 10)
+    print(p2.zeige_lebenspunkte()) # 32
