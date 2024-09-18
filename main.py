6b314c1804eea5d743059a34c5b80d59d8d0ebd4
Christoph <c.wagner.germany@gmail.com>
2024-09-18 17:09:59 +0200

    obj10

--- main.py
+++ main.py
@@ -1,55 +1,7 @@
-# class Pokemon:
-    
-#     def __init__(self, name):
-#         self.__n = name
-#         self.__lebenspunkte = 42
-#         self.__level = 1
-        
-#     # Magische Methode
-#     def __str__(self):
-#         return f"Name: {self.__n}\nLebenspunkte: {self.__lebenspunkte}\nLevel: {self.__level}"
-    
-#     def __gt__(self, other):
-#         return self.__level > other.__level
-        
-#     def vorstellen(self):
-#         print(f"{self.__n}, {self.__n}!")
-        
-#     def zeige_lebenspunkte(self):
-#         return self.__lebenspunkte
-    
-#     def zeige_level(self):
-#         print(f"{self.__n} :: {self.__level}")
-    
-#     def entwickeln(self):
-#         self.__level += 1
-        
-#     def attackieren(self, other, schaden):
-#         other.__lebenspunkte -= schaden
-        
-                
-# if __name__ == "__main__":
-#     p1 = Pokemon("Pikachu")
-#     p2 = Pokemon("Bisasam")
-    
-#     p1.entwickeln()
-    
-#     print(p1 > p2) # True
-    
-#     str(p1)
-    
-class Pokemon:
-    
-    def __init__(self, name):
-        self.name = name
-        self.lebenspunkte = 42
-        self.level = 1
-        
-    def vorstellen(self): # Methoden Definition
-        print(f"{self.name}, {self.name}!")
-        
-bisasam = Pokemon("Bisasam")
-bisasam.vorstellen() # Methoden Aufruf
+liste = [1, 2, 3, "Flo", "Junus", "Manu"]
 
-len("asldkfjlsdkjflikds")
-len([1, 2, 3])
+try:
+    index = int(input("Bitte gib einen Index ein: "))
+    print(liste[index])
+except IndexError:
+    print("Der Zugriff ist fehlgeschlagen.")
