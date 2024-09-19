c89c97537a353d21ae5a34453f81a43f67f4adf8
Christoph <c.wagner.germany@gmail.com>
2024-09-19 15:59:19 +0200

    try8

--- main.py
+++ main.py
@@ -16,3 +16,12 @@ except Exception:
     pass
 finally:
     f.close()
+    
+    
+try:
+    zahl = int(input("Gib eine Zahl ein: "))
+    print(10 / zahl)
+except ValueError:
+    print("Bitte gib eine ganze Zahl ein.")
+except ZeroDivisionError:
+    print("Division durch Null ist nicht erlaubt.")
