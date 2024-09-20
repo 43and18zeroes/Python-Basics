5b3ea2cd5dc3f707ff4afa0b07a5441ac36e7b8d
Christoph <c.wagner.germany@gmail.com>
2024-09-20 17:18:20 +0200

    exc6

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 try:
-    liste = [1, 2, 3]
-    element = liste[3]
-except IndexError:
-    print("Index außerhalb des Bereichs.")
-else:
-    print("Element gefunden:", element)
+    try:
+        # Innerer try-Block
+        x = 10 / 0
+    except ZeroDivisionError:
+        print("Division durch Null im inneren Block.")
+except Exception as e:
+    print("Äußerer Fehler:", e)
