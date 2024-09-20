7178b6c0c5faa83ed8e9a73df8dd01ac19e9a1f1
Christoph <c.wagner.germany@gmail.com>
2024-09-20 20:45:35 +0200

    exc9

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def sicheres_teilen(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return "Division durch Null"
-
-print(sicheres_teilen(10, 0))
+try:
+    zahl = int(input("Gib eine Zahl ein: "))
+    print(10 / zahl)
+except ValueError:
+    print("Bitte gib eine ganze Zahl ein.")
+except ZeroDivisionError:
+    print("Division durch Null ist nicht erlaubt.")
