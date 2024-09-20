f3a69413ae760e06d29e8ba693204e587427c3ae
Christoph <c.wagner.germany@gmail.com>
2024-09-20 21:22:34 +0200

    exc12

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def sicheres_teilen(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return "Division durch Null"
-
-print(sicheres_teilen(10, 0))
+try:
+    liste = [1, 2, 3]
+    element = liste[3]
+except IndexError:
+    print("Index außerhalb des Bereichs.")
+else:
+    print("Element gefunden:", element)
