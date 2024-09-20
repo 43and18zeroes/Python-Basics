c40c701db454ed54d176838478029b8869917fa9
Christoph <c.wagner.germany@gmail.com>
2024-09-20 20:54:53 +0200

    exc10

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 try:
-    zahl = int(input("Gib eine Zahl ein: "))
-    print(10 / zahl)
-except ValueError:
-    print("Bitte gib eine ganze Zahl ein.")
-except ZeroDivisionError:
-    print("Division durch Null ist nicht erlaubt.")
+    datei = open("nicht_existierende_datei.txt", "r")
+    inhalt = datei.read()
+except FileNotFoundError:
+    print("Datei nicht gefunden.")
+except Exception as e:
+    print("Ein unerwarteter Fehler ist aufgetreten:", e)
+finally:
+    datei.close()  # Wird immer ausgeführt, auch bei Fehlern
