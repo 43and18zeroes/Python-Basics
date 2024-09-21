28ad787a7d160badcf4be4a24666c25445ce8a4a
Christoph <c.wagner.germany@gmail.com>
2024-09-21 23:45:19 +0200

    adv11

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-even_squares = [x**2 for x in numbers if x % 2 == 0]
-print(even_squares)  # Ausgabe: [4, 16]
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))  # Ausgabe: 120
