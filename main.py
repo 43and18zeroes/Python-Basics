9c38ec8861103c554c89c7061e99ab16f4d8ce30
Christoph <c.wagner.germany@gmail.com>
2024-09-21 16:50:23 +0200

    adv2

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-def my_decorator(func):
-    def wrapper(*args, **kwargs):
-        print("Vor Ausführung der Funktion")
-        result = func(*args, **kwargs)
-        print("Nach Ausführung der Funktion")
-        return result
-    return wrapper
-
-@my_decorator
-def greet(name):
-    print(f"Hallo, {name}!")
-
-greet("Welt")
+squares = (x**2 for x in range(10))
+print(next(squares))  # Ausgabe: 0
