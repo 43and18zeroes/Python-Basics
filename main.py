1709d9eedf8915b21d3310ab5466331f1290f6b4
Christoph <c.wagner.germany@gmail.com>
2024-09-25 02:56:54 +0200

    md4

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-class FileExistsError(Exception):
-    def __init__(self, filename):
-        super().__init__(f"File '{filename}' already exists.")
-        self.filename = filename
-
 def create_file_safely(filename):
     try:
         with open(filename, 'w') as f:
