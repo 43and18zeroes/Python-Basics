a8b217e9d650ad16d9a2e02395c2efe55067e35f
Christoph <c.wagner.germany@gmail.com>
2024-09-26 19:39:54 +0200

    py8

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-def count_words_in_file(filename):
-  word_count = 0
-  with open(filename, 'r') as file:
-    for line in file:
-      words = line.split()
-      word_count += len(words)
-  return word_count
+class Dog:
+  def __init__(self, name, breed):
+    self.name = name
+    self.breed = breed
 
-# Beispiel-Datei
-filename = "my_text_file.txt"
-total_words = count_words_in_file(filename)
-print("Anzahl der Wörter:", total_words)
+  def bark(self):
+    print("Woof!")
+
+# Erstellen eines Objekts
+my_dog = Dog("Buddy", "Golden Retriever")
+my_dog.bark()
