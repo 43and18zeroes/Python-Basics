8e463db0c1aeaf72eec66c0f17a523c6e7b29d13
Christoph <c.wagner.germany@gmail.com>
2024-09-26 21:08:29 +0200

    py13

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import pandas as pd
+
 
 # Erstellen eines DataFrames
 data = {'Name': ['Alice', 'Bob', 'Charlie'],
         'Alter': [25, 30, 28]}
-df = pd.DataFrame(data)
 
-# Daten anzeigen
-print(df)
+
