c224b49acf778c7342832793191a2b177500017c
Christoph <c.wagner.germany@gmail.com>
2024-09-28 17:21:52 +0200

    mt12

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import random
-from datetime import datetime, timedelta
 
-def random_date(start_date, end_date):
-    delta = end_date - start_date
-    random_days = random.randint(0, delta.days)
-    return start_date + timedelta(days=random_days)
+def random_boolean():
+    return random.choice([True, False])
 
-start = datetime(2023, 1, 1)
-end = datetime(2024, 1, 1)
-print(random_date(start, end))  # Zufälliges Datum im Jahr 2023
+print(random_boolean())  # Gibt True oder False zurück
