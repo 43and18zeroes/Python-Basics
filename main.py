097870d25558dc5b2780e2acfc8a8cd20ec9b367
Christoph <c.wagner.germany@gmail.com>
2024-09-28 17:06:11 +0200

    mt11

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random
+from datetime import datetime, timedelta
 
-def random_passphrase(words, word_count):
-    return ' '.join(random.choices(words, k=word_count))
+def random_date(start_date, end_date):
+    delta = end_date - start_date
+    random_days = random.randint(0, delta.days)
+    return start_date + timedelta(days=random_days)
 
-word_list = ['Hund', 'Katze', 'Maus', 'Auto', 'Baum', 'Haus']
-print(random_passphrase(word_list, 4))  # Zufällige Passphrase mit 4 Wörtern
+start = datetime(2023, 1, 1)
+end = datetime(2024, 1, 1)
+print(random_date(start, end))  # Zufälliges Datum im Jahr 2023
