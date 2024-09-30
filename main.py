1d44cd13be5341398968af4b18bbf20b89406522
Christoph <c.wagner.germany@gmail.com>
2024-09-30 21:07:45 +0200

    my9

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from dataclasses import dataclass
+import concurrent.futures
+import time
 
-@dataclass
-class Point:
-    x: int
-    y: int
+def sleep_and_return(sec):
+    time.sleep(sec)
+    return sec
 
-p = Point(10, 20)
-print(p)
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = [executor.submit(sleep_and_return, i) for i in range(5)]
+    for future in concurrent.futures.as_completed(futures):
+        print(future.result())
