403b0234b8975a7ee939730f2659d6f658ae556c
Christoph <c.wagner.germany@gmail.com>
2024-09-30 18:49:00 +0200

    my5

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def countdown(n):
-    while n > 0:
-        yield n
-        n -= 1
+def my_decorator(func):
+    def wrapper():
+        print("Something before the function.")
+        func()
+        print("Something after the function.")
+    return wrapper
 
-for number in countdown(5):
-    print(number)
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
