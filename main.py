8557539e227ef76236e717d00485c7f78c773259
Christoph <c.wagner.germany@gmail.com>
2024-09-30 22:27:19 +0200

    my11

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import importlib
+class MyRange:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
 
-math_module = importlib.import_module('math')
-print(math_module.sqrt(16))
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.current >= self.end:
+            raise StopIteration
+        self.current += 1
+        return self.current - 1
+
+my_range = MyRange(1, 5)
+for num in my_range:
+    print(num)
