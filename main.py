1114cdca6a4eb9447ff5765976cfdb67bb65c13f
Christoph <c.wagner.germany@gmail.com>
2024-10-01 19:44:42 +0200

    py10

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-class ManagedFile:
-    def __init__(self, filename):
-        self.filename = filename
-
-    def __enter__(self):
-        self.file = open(self.filename, 'r')
-        return self.file
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.file.close()
-
-with ManagedFile('my_file.txt') as f:
-    for line in f:
-        print(line)
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
