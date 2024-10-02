32611ea2f14562b5064226fef3b915d67c7b174f
Christoph <c.wagner.germany@gmail.com>
2024-10-02 17:41:13 +0200

    sd8

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+def apply_to_all(func, iterable):
+    return [func(item) for item in iterable]
+
+numbers = [1, 2, 3, 4]
+squared = apply_to_all(lambda x: x**2, numbers)
