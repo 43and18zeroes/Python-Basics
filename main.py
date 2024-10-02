9d9c5f726519e6f7512e98c4568289d6c322ba3a
Christoph <c.wagner.germany@gmail.com>
2024-10-02 17:48:46 +0200

    sd7

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def apply_to_all(func, iterable):
-    return [func(item) for item in iterable]
-
-numbers = [1, 2, 3, 4]
-squared = apply_to_all(lambda x: x**2, numbers)
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
