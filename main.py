d4b7309b8c13030736e7e7eafab343877417de2a
Christoph <c.wagner.germany@gmail.com>
2024-10-02 22:14:08 +0200

    sd13

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+class ManagedFile:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        self.file = open(self.filename, 'r')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+with ManagedFile('my_file.txt') as f:
+    for line in f:
+        print(line)
