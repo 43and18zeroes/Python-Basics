fe14f7fbddad4143ce8149e3f8839bb5bf5ab1af
Christoph <c.wagner.germany@gmail.com>
2024-10-04 22:28:35 +0200

    json12

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 5, 4, 5]
-
-plt.plot(x, y)
-plt.xlabel('X-Achse')
-plt.ylabel('Y-Achse')
-plt.title('Einfacher Plot')
-plt.show()
+data = {'Name': ['Alice', 'Bob', 'Charlie'], 'Alter': [25, 30, 28]}
+df = pd.DataFrame(data)
+print(df)
