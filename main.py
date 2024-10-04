5caa8f71f66e1a5d1208c5289a3d7f5422036982
Christoph <c.wagner.germany@gmail.com>
2024-10-04 17:18:46 +0200

    json4

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from scipy.integrate import quad
+import random
 
-# Funktion integrieren
-def f(x):
-    return x**2 * np.exp(-x)
+def number_guessing_game():
+    secret_number = random.randint(1, 20)
+    guesses_taken = 0
 
-result, error = quad(f, 0, np.inf)
-print(result)
+    print('Ich habe mir eine Zahl zwischen 1 und 20 ausgedacht.')
+
+    while guesses_taken < 6:
+        guess = int(input('Rate mal: '))
+        guesses_taken += 1
+
+        if guess < secret_number:
+            print('Deine Zahl ist zu klein.')
+        elif guess > secret_number:
+            print('Deine Zahl ist zu groß.')
+        else:
+            break
+
+    if guess == secret_number:
+        print('Gut gemacht! Du hast die Zahl in ' + str(guesses_taken) + ' Versuchen erraten!')
+    else:
+        print('Leider verloren. Die Zahl war ' + str(secret_number))
+
+number_guessing_game()
