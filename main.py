b58b56c0f0d35b17af1d5a0a16b336b252376cd4
Christoph <c.wagner.germany@gmail.com>
2024-10-04 18:17:22 +0200

    json5

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-import random
-
-def number_guessing_game():
-    secret_number = random.randint(1, 20)
-    guesses_taken = 0
-
-    print('Ich habe mir eine Zahl zwischen 1 und 20 ausgedacht.')
-
-    while guesses_taken < 6:
-        guess = int(input('Rate mal: '))
-        guesses_taken += 1
-
-        if guess < secret_number:
-            print('Deine Zahl ist zu klein.')
-        elif guess > secret_number:
-            print('Deine Zahl ist zu groß.')
-        else:
-            break
-
-    if guess == secret_number:
-        print('Gut gemacht! Du hast die Zahl in ' + str(guesses_taken) + ' Versuchen erraten!')
+def fibonacci(n):
+    """Gibt die n-te Fibonacci-Zahl zurück."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
     else:
-        print('Leider verloren. Die Zahl war ' + str(secret_number))
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
-number_guessing_game()
+n = int(input("Gib eine Zahl ein: "))
+for i in range(n):
+    print(fibonacci(i))
