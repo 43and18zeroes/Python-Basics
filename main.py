94ddbe1cfe15ea27f6ec532c406ee2509862c92c
Christoph <c.wagner.germany@gmail.com>
2024-10-05 22:57:34 +0200

    arr11

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def transponieren(matrix):
-    """Transponiert eine Matrix (2D-Array)."""
-    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+def produkt(array1, array2):
+    """Berechnet das elementweise Produkt zweier Arrays gleicher Länge."""
+    if len(array1) != len(array2):
+        raise ValueError("Arrays müssen dieselbe Länge haben")
+    return [x*y for x, y in zip(array1, array2)]
