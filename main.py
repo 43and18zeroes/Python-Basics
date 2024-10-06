dc54864c5f5afde53459dcd0b8f2423fa78c4097
Christoph <c.wagner.germany@gmail.com>
2024-10-06 16:45:18 +0200

    json10

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-def timing_decorator(func):
-    import time
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print(f"{func.__name__} took {end - start} seconds")
-        return result
-    return wrapper
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
 
-@timing_decorator
-def slow_function():
-    import time
-    time.sleep(1)
-
-slow_function()
+fib = fibonacci()
+print([next(fib) for _ in range(10)])
