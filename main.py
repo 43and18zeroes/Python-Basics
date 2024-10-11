c647374136a8c0b36818e919da63c6bf59e62130
Christoph <c.wagner.germany@gmail.com>
2024-10-11 23:46:03 +0200

    max9

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import numpy as np
 
-def portfolio_return(returns):
-  """Berechnet die durchschnittliche Rendite eines Portfolios."""
-  return np.mean(returns)
+def volatility(returns):
+  """Berechnet die Volatilität (Standardabweichung) einer Rendite."""
+  return np.std(returns)
