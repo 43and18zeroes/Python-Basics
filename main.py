705298f9b7bf3dc232f33685b3f1e324c312a541
Christoph <c.wagner.germany@gmail.com>
2024-10-13 20:34:42 +0200

    slug$

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def render_template(**template_vars):
-    # Simulierte Templating-Engine
-    template = "Hello, {name}! Your age is {age}."
-    print(template.format(**template_vars))
-
-render_template(name="Alice", age=25)
+def summe(array):
+    """Berechnet die Summe aller Elemente in einem Array."""
+    return sum(array)
