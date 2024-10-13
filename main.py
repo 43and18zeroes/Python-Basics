7082b678ddeaa7b4dbc7d2d87a86f346d2f68333
Christoph <c.wagner.germany@gmail.com>
2024-10-13 21:32:44 +0200

    slug7

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-def verdoppeln(array):
-    """Verdoppelt jedes Element in einem Array."""
-    return [x * 2 for x in array]
+def quadrate(array):
+    """Berechnet die Quadratzahlen aller Elemente in einem Array."""
+    return [x**2 for x in array]
