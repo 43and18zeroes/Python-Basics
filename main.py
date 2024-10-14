f60d3b71c3841bdecd61ba4ad389535893d09130
Christoph <c.wagner.germany@gmail.com>
2024-10-14 09:13:48 +0200

    rnd1

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def produkt(array1, array2):
-    """Berechnet das elementweise Produkt zweier Arrays gleicher Länge."""
-    if len(array1) != len(array2):
-        raise ValueError("Arrays müssen dieselbe Länge haben")
-    return [x*y for x, y in zip(array1, array2)]
+import random
+zufallszahl = random.random()
+print(zufallszahl)
